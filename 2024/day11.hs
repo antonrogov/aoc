c8e82ad2803f5cc46a0e7014584747f0cd0f79dc
Anton Rogov <anton@blackbits.pro>
2024-12-11 17:23:31 +0300

    2024 day 11 part 2

--- 2024/day11.hs
+++ 2024/day11.hs
@@ -5,9 +5,15 @@ stone s
                              in [read $ take l2 ss, read $ drop l2 ss]
   | otherwise = [s * 2024]
 
-process ss = concat $ map stone ss
+inc n acc s = case lookup s acc of
+  Just n' -> (s, n' + n) : filter (\ (s', _) -> s' /= s) acc
+  Nothing -> (s, n) : acc
+
+process cs =
+  foldl (\acc (s, c) -> foldl (inc c) acc $ stone s) [] cs
 
 main = do
    text <- readFile "day11.txt"
    let ss = (map read $ words text) :: [Integer]
-   print $ length $ iterate process ss !! 25
+   let cs = foldl (inc 1) [] ss
+   print $ sum $ map snd $ iterate process cs !! 75
